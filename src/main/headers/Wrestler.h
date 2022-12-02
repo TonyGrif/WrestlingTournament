0b79b890efa2e83e6d5195167b47cd64d6198fdb
TonyGrif <TonyGriffin2000@gmail.com>
2022-12-01 22:47:05 -0500

    basic wrestler variables

---
+++ src/main/headers/Wrestler.h
@@ -0,0 +1,33 @@
+#ifndef WRESTLER_H
+#define WRESTLER_H
+
+/**
+ * @brief 
+ * 
+ */
+class Wrestler
+{
+    public:
+
+    private:
+        /**
+         * @brief Wrestler's age.
+         * 
+         * Stores a integer between 13 and 17.
+         */
+        int age;
+
+        /**
+         * @brief Wrestler's weight.
+         * 
+         * Determined in part from the age.
+         */
+        int weight;
+
+        /**
+         * @brief Determines a wrestler's skill.
+         */
+        int abilityScore;
+};
+
+#endif
